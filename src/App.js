9ab80574b41cc5be023628b49babf79b5e316e01
hojncode <glicm12@gmail.com>
2023-04-16 17:42:36 +0900

    Props 비구조화할당으로 받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+function Food({ fav }) {
+  // {fav}는 props.fav 와 같다 = es6 문법.비구조화 할당
+  return (
+    <>
+      <h1>{fav}</h1>
+    </>
+  );
+}
+
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div>
+      <Food fav="chicken" />
+      <Food fav="kimchi" />
+    </div>
+  );
 }
 
 export default App;
