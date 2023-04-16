4d570921b40b9e4d7fc23f706a161058ed733610
hojncode <glicm12@gmail.com>
2023-04-16 18:26:14 +0900

    객체에서 요소  가져오기 - map, id, alt, props

--- src/App.js
+++ src/App.js
@@ -1,8 +1,44 @@
-function Food({ fav }) {
+import React from "react";
+
+const foodILike = [
+  {
+    id: 1,
+    name: "Kimchi",
+    image:
+      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
+  },
+  {
+    id: 2,
+    name: "Samgyeopsal",
+    image:
+      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
+  },
+  {
+    id: 3,
+    name: "Bibimbap",
+    image:
+      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
+  },
+  {
+    id: 4,
+    name: "Doncasu",
+    image:
+      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
+  },
+  {
+    id: 5,
+    name: "Kimbap",
+    image:
+      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
+  },
+];
+
+function Food({ name, picture }) {
   // {fav}는 props.fav 와 같다 = es6 문법.비구조화 할당
   return (
     <>
-      <h1>{fav}</h1>
+      <h1>{name}</h1>
+      <img alt="name" src={picture} />
     </>
   );
 }
@@ -10,8 +46,11 @@ function Food({ fav }) {
 function App() {
   return (
     <div>
-      <Food fav="chicken" />
-      <Food fav="kimchi" />
+      <div>
+        {foodILike.map((item) => (
+          <Food key={item.id} name={item.name} picture={item.image} />
+        ))}
+      </div>
     </div>
   );
 }
