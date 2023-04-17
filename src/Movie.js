c0afe32fbb777f50c71cfff793b30730d2487538
hojncode <glicm12@gmail.com>
2023-04-17 15:12:38 +0900

    css 파일 추가

--- src/Movie.js
+++ src/Movie.js
@@ -1,8 +1,20 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./Movie.css";
 
-function Movie({ id, year, title, summary, poster }) {
-  return <h5>{title}</h5>;
+function Movie({ year, title, summary, poster }) {
+  return (
+    <div className="moive">
+      <div className="movie__data">
+        <img src={poster} alt={title} title={title} />
+        <h3 className="movie__title" style={{ backgroundColor: "red" }}>
+          {title}
+        </h3>
+        <h5 className="movie__year">{year}</h5>
+        <p className="movie__summary">{summary}</p>
+      </div>
+    </div>
+  );
 }
 
 Movie.propTypes = {
